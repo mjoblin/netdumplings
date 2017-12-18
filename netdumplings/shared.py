3a0d19214195286ada26d885018058a4d22637fe
Michael Joblin <mike@tastycheese.local>
2017-12-17 23:51:53 -0800

    Remove support for config, and update approach to logging

--- netdumplings/shared.py
+++ netdumplings/shared.py
@@ -11,74 +11,49 @@ ND_CLOSE_MSGS = {
     'eater_full': (4102, "dumpling eater is full"),
 }
 
+LOGGING_CONFIG_FILE = os.path.join(
+    os.path.dirname(__file__), 'data', 'logging.json'
+)
+DEFAULT_SHIFTY_HOST = 'localhost'
+DEFAULT_SHIFTY_IN_PORT = 11347
+DEFAULT_SHIFTY_OUT_PORT = 11348
+SHIFTY_STATUS_FREQ = 5
 
-def get_config_file():
+
+def configure_logging(log_level=logging.INFO, config_file=LOGGING_CONFIG_FILE):
     """
-    Returns the path to the NetDumplings config file.
+    Configure logging. Configuration is retrieved from an external
+    file (``data/logging.json`` in the netdumplings package), which can be
+    overridden with ``config_file`` or with the `NETDUMPLINGS_LOGGING_CONFIG``
+    environment variable.
 
-    :return: Path to config file.
+    :param log_level: Log level to use. Defaults to ``'INFO'``.
+    :param config_file: Path to the logging config file to use. Defaults to
+        ``data/logging.json`` in the netdumplings package.
     """
-    return os.path.join(os.path.dirname(__file__), 'data', 'config.json')
-
-
-def get_config(file=None):
-    """
-    Returns the NetDumplings configuration data.
-
-    :param file: Path to config; ``None`` falls back on default config.
-    :return: A dict of configuration data.
-    """
-    config_file = get_config_file() if not file else file
+    use_basic_config = False
 
     try:
-        with open(config_file) as f:
-            config = json.load(f)
-    except (IOError, json.decoder.JSONDecodeError) as e:
-        raise NetDumplingsError("Error loading config from {0}: {1}".format(
-            config_file, e))
+        config_file = os.environ['NETDUMPLINGS_LOGGING_CONFIG']
+    except KeyError:
+        pass
 
-    return config
-
-
-def get_logging_config_file():
-    """
-    Returns the path to the logging config file.
-
-    :return: Path to logging config file.
-    """
-    return os.path.join(os.path.dirname(__file__), 'data', 'logging.json')
-
-
-def configure_logging(log_level=None, config_file=None, logger_name=None):
-    """
-    Configure logging.
-
-    :param log_level: Log level to use.  ``None`` falls back on NetDumplings
-        default logging level.
-    :param config_file: Path to the logging config file to use.  Defaults to
-        file returned by :func:`get_logging_config_file`.
-    :param logger_name: Name of the logger to configure.  Defaults to
-        ``netdumplings``.
-    """
-    if config_file is None:
-        config_file = get_logging_config_file()
-
-    try:
-        with open(config_file) as f:
-            logging_config = json.load(f)
-
-        logging.config.dictConfig(logging_config)
-    except (IOError, json.decoder.JSONDecodeError) as e:
-        print("error loading logging config '{0}': {1}".format(config_file, e))
-        logging.basicConfig()
-
-    if log_level:
-        log = logging.getLogger(logger_name if logger_name else 'netdumplings')
+    if os.path.exists(config_file):
         try:
-            log.setLevel(
-                log_level.upper() if isinstance(log_level, str) else log_level)
-        except ValueError as e:
-            log.warning("logging error: {0}".format(e))
+            with open(config_file) as logging_config_handler:
+                logging_config = json.load(logging_config_handler)
+
+            logging.config.dictConfig(logging_config)
+        except (IOError, json.decoder.JSONDecodeError) as e:
+            use_basic_config = True
+            print(
+                'error loading logging config: {}: {}'.format(config_file, e)
+            )
+    else:
+        use_basic_config = True
+
+    if use_basic_config:
+        logging.basicConfig(level=log_level)
 
 
 def validate_dumpling(dumpling_json):
