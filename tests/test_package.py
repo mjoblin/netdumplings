30cc4cb8e769fcd57db8bf5eae61228e060b7f78
Michael Joblin <mike@tastycheese.local>
2017-12-23 21:22:10 -0800

    Add package tests

---
+++ tests/test_package.py
@@ -0,0 +1,11 @@
+import netdumplings
+from netdumplings._version import __version__
+
+
+class TestPackage:
+    def test_version(self):
+        """
+        Test that the __version__ attribute is found on the package and that
+        it matches the version in netdumplings/_version.py.
+        """
+        assert netdumplings.__version__ == __version__
