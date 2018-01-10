f58f0483bb31f4630a1937ca6156ca95ad66043f
Michael Joblin <mike@tastycheese.local>
2018-01-09 22:04:30 -0800

    Remove dumpling queue/send and poke awareness from DumplingChef
    This logic is now managed by the DumplingKitchen.

--- netdumplings/dumplingchef.py
+++ netdumplings/dumplingchef.py
@@ -13,8 +13,9 @@ class DumplingChef:
     you'll normally subclass DumplingChef and let nd-snifty take care of
     instantiating it.**
 
-    When instantiated, a DumplingChef registers its :meth:`packet_handler` and
-    :meth:`interval_handler` with the given ``kitchen``.
+    When instantiated, a DumplingChef registers itself with the given
+    ``kitchen`` which will take care of calling the chef's packet and interval
+    handlers.
 
     A DumplingChef can create many :class:`~Dumpling` objects, where a
     dumpling is a tasty bundle of information which (usually) describes some
@@ -22,8 +23,8 @@ class DumplingChef:
     have been sniffed so far; how many packets of various network layers have
     been sniffed; etc.
 
-    This class implements the following methods which can also be overridden
-    by subclasses:
+    This class implements the following methods which will usually be
+    overridden by subclasses:
 
     * :meth:`packet_handler`
     * :meth:`interval_handler`
@@ -32,109 +33,52 @@ class DumplingChef:
     # chef cannot be assigned to any kitchens via snifty.
     assignable_to_kitchen = True
 
-    _epoch = datetime.datetime.utcfromtimestamp(0)
-
-    def __init__(self, kitchen=None, dumpling_queue=None, receive_pokes=False):
+    def __init__(self, kitchen=None):
         """
         :param kitchen: The :class:`DumplingKitchen` which is providing the
             network packet ingredients used to create the dumplings.
-        :param dumpling_queue: The :class:`multiprocessing.Queue` to send fresh
-            new dumplings to.
-        :param receive_pokes: Whether this chef wants to receive time-interval
-            pokes to its interval_handler.
         """
         self.kitchen = kitchen
-        self.dumpling_queue = dumpling_queue
-        self.receive_pokes = receive_pokes
-
         self.name = type(self).__name__
         self.dumplings_sent_count = 0
         self._logger = logging.getLogger(__name__)
 
         if self.kitchen:
-            interval_handler = \
-                self.interval_handler if self.receive_pokes else False
-
-            self.kitchen.register_handler(
-                chef_name=self.name,
-                packet_handler=self.packet_handler,
-                interval_handler=interval_handler
-            )
+            self.kitchen.register_chef(self)
 
     def __repr__(self):
-        return (
-            '{}('
-            'kitchen={}, '
-            'dumpling_queue={}, '
-            'receive_pokes={})'.format(
-                type(self).__name__,
-                repr(self.kitchen),
-                repr(self.dumpling_queue),
-                self.receive_pokes,
-            )
-        )
+        return '{}(kitchen={})'.format(type(self).__name__, repr(self.kitchen))
 
     def packet_handler(self, packet):
         """
-        This handler is called automatically by `nd-snifty` whenever a new
-        packet has been sniffed.
+        Called automatically by `nd-snifty` whenever a new packet has been
+        sniffed.
 
-        This method is expected to be overridden by child classes.
-
-        This base implementation immediately sends a dumpling, the payload of
-        which will be a string representation of the packet.
+        This method is expected to be overridden by child classes. This base
+        implementation returns a payload which is the string representation of
+        the packet.
 
         :param packet: Network packet (from scapy).
+        :return: Dumpling payload.
         """
         payload = "{0}: {1}".format(type(self).__name__, packet.summary())
         self._logger.debug("{0}: Received packet: {1}",
                            self.name, packet.summary())
-        self.send_packet_dumpling(payload)
+
+        return payload
 
     def interval_handler(self, interval=None):
         """
-        This handler is called automatically at regular intervals by
-        `nd-snifty`.  Allows for time-based (rather than purely packet-based)
-        chefs to keep on cheffing even in the absence of fresh packets.
+        Called automatically at regular intervals by `nd-snifty`.  Allows for
+        time-based (rather than purely packet-based) chefs to keep on cheffing
+        even in the absence of fresh packets.
 
-        This method is expected to be overridden by child classes.
-
-        This base implementation does nothing.
+        This method is expected to be overridden by child classes. This base
+        implementation does nothing.
 
         :param interval: Frequency (in seconds) of the time interval pokes.
         """
         self._logger.debug(
             "{0}: Received interval_handler poke", self.name)
 
-    def _send_dumpling(self, payload, driver):
-        """
-        Initiates a send of a dumpling to all the dumpling eaters.
-
-        :param payload: The dumpling payload.  Can be anything which is JSON
-            serializable.  It's up to the dumpling eaters to make sense of it.
-        :param driver: The DumplingDriver; either ``DumplingDriver.packet`` or
-            ``DumplingDriver.interval``.
-        """
-        dumpling = Dumpling(chef=self, driver=driver, payload=payload)
-        dumpling_json = dumpling()
-        self.dumpling_queue.put(dumpling_json)
-        self._logger.debug("{0}: Sent dumpling, {1} bytes".format(
-            self.name, len(dumpling_json)))
-
-    def send_interval_dumpling(self, payload):
-        """
-        Initiates a send of an interval dumpling to all dumpling eaters.
-
-        :param payload: The dumpling payload.  Can be anything which is JSON
-            serializable.  It's up to the dumpling eaters to make sense of it.
-        """
-        self._send_dumpling(payload, driver=DumplingDriver.interval)
-
-    def send_packet_dumpling(self, payload):
-        """
-        Initiates a send of a packet dumpling to all dumpling eaters.
-
-        :param payload: The dumpling payload.  Can be anything which is JSON
-            serializable.  It's up to the dumpling eaters to make sense of it.
-        """
-        self._send_dumpling(payload, driver=DumplingDriver.packet)
+        return None
